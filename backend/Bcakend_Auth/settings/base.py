227539beb9ba17eae5c908266015cb8b947566db
Ankul Maurya <ankulmaurya88@gmail.com>
2026-02-23 18:06:43 +0530

    update

--- backend/Bcakend_Auth/settings/base.py
+++ backend/Bcakend_Auth/settings/base.py
@@ -30,6 +30,28 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
+
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
